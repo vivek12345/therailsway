2b9c8e0454ffd4b185386511bb858c089667a58e
Vivek <vivek.of.nayyar@gmail.com>
2013-07-07 17:17:17 +0530

    changes to add admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,24 +14,12 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		self.current_user=nil
-		cookies.delete(:remember_token)
+		sign_out
 		flash[:success]="Successfully signed-out"
 		redirect_to root_url
 	end
-	def sign_in(user)
-		cookies.permanent[:remember_token]=user.remember_token
-		self.current_user=user
-	end
-	def current_user=(user)
-    @current_user = user
+	def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
-
-  	def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
-  	end
-	def signed_in?
-		!current_user.nil?
-	end
-
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	before_save{ |user| user.email=email.downcase}
+	before_save{ |user| user.admin=true}
 	before_save :create_remember_token
 	validates :name,presence: true,length:{maximum:50}
 	VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
