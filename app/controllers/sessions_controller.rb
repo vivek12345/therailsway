585001ab0d90a59b79de334e8058d1cde39c29b7
Vivek <vivek.of.nayyar@gmail.com>
2013-07-07 12:45:31 +0530

    Change to make admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,24 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		sign_out
+		self.current_user=nil
+		cookies.delete(:remember_token)
 		flash[:success]="Successfully signed-out"
 		redirect_to root_url
 	end
-	
+	def sign_in(user)
+		cookies.permanent[:remember_token]=user.remember_token
+		self.current_user=user
+	end
+	def current_user=(user)
+    @current_user = user
+    end
+
+  	def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  	end
+	def signed_in?
+		!current_user.nil?
+	end
+
 end
